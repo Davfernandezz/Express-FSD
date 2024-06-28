8d01e519f21dab784a86d6793e9fdb8b700babf8
David Fernadez <daffevafernandez@gmail.com>
2024-06-28 10:40:58 +0200

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/healthy', (req, res)=>{
+res.send('Server is healthy');
+})
+
+app.listen(4000, () =>{
+    console.log('server is running');
+})
