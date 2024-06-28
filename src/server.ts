b470c616cb077bbd0a2e03286a0cc05b56be7627
David Fernadez <daffevafernandez@gmail.com>
2024-06-28 12:01:43 +0200

    CRUD added

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,22 @@ app.get('/healthy', (req, res)=>{
 res.send('Server is healthy');
 })
 
+app.get('/books', (req, res)=>{
+    res.send('GET ALL BOOKS')
+})
+
+app.post('/books', (req, res)=>{
+    res.send('BOOK CREATED')
+})
+
+app.put('/books', (req, res)=>{
+    res.send('BOOK UPDATED')
+})
+
+app.delete('/books', (req, res)=>{
+    res.send('BOOK DELETED')
+})
+
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
 })
