e30c8a82cd65027e7cca159d06b18f84dad81fb3
David Fernadez <daffevafernandez@gmail.com>
2024-06-28 11:08:44 +0200

    port 4000 to const

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.PORT || 4000
+
 app.get('/healthy', (req, res)=>{
 res.send('Server is healthy');
 })
 
-app.listen(4000, () =>{
-    console.log('server is running');
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
 })
