a6b3f6c5dd4625a61f1a470ea7c6e408974b1123
David Fernadez <daffevafernandez@gmail.com>
2024-06-28 12:42:11 +0200

    Authors CRUD

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,33 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json())
+
 const PORT = process.env.PORT || 4000
 
 app.get('/healthy', (req, res)=>{
 res.send('Server is healthy');
 })
+
+
+app.post('/authors', (req, res)=>{
+console.log(req.body);
+console.log(req.body.name);
+console.log(req.body.nationality);
+
+    res.send('authors CREATED')
+})
+
+
+app.put('/authors/:id', (req, res)=>{
+    console.log(req.params.id)
+    res.send(`authors UPDATED with id: ${req.params.id}`)
+})
+
+app.delete('/authors/:id', (req, res)=>{
+    res.send(`authors DELETED with id: ${req.params.id}`)
+})
+
 
 app.get('/books', (req, res)=>{
     res.send('GET ALL BOOKS')
