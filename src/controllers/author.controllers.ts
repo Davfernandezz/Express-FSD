a805296db404d26402d8606d9abfd3bcf9b0c0f4
David Fernadez <daffevafernandez@gmail.com>
2024-07-02 11:01:24 +0200

    create a models

--- src/controllers/author.controllers.ts
+++ src/controllers/author.controllers.ts
@@ -1,14 +1,23 @@
 import { Request, Response } from "express";
+import { Author } from "../database/models/Author";
 
 //POST
-export const createAuthor = (req: Request, res: Response) => {
+export const createAuthor = async (req: Request, res: Response) => {
     console.log(req.body);
     console.log(req.body.name);
     console.log(req.body.nationality)
 
+    const newAuthor = await Author.create(
+        {
+            name: "datata",
+            nationality: "spain"
+        }
+    ).save();
+
     res.json({
         success: true,
-        message: 'CREATE AUTHOR'
+        message: 'Author created succesfully',
+        data: newAuthor
     })
 }
 
--- src/database/db.ts
+++ src/database/db.ts
@@ -5,6 +5,7 @@ import { Author1719825063867 } from "./migrations/1719825063867-author";
 import { User1719830267980 } from "./migrations/1719830267980-user";
 import { Book1719832355608 } from "./migrations/1719832355608-book";
 import { Favourite1719906554069 } from "./migrations/1719906554069-favourite";
+import { Author } from "./models/Author";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -13,7 +14,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    entities: [],
+    entities: [Author],
     migrations: [Author1719825063867, User1719830267980, Book1719832355608, Favourite1719906554069],
     synchronize: false,
     logging: false,
