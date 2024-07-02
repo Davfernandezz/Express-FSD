a700c7f63440d59fab3fe16e09bb592f0b10dd8a
David Fernadez <daffevafernandez@gmail.com>
2024-07-02 10:10:22 +0200

    favourites migration

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,6 +4,7 @@ import { DataSource } from "typeorm";
 import { Author1719825063867 } from "./migrations/1719825063867-author";
 import { User1719830267980 } from "./migrations/1719830267980-user";
 import { Book1719832355608 } from "./migrations/1719832355608-book";
+import { Favourite1719906554069 } from "./migrations/1719906554069-favourite";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -13,7 +14,7 @@ export const AppDataSource = new DataSource({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: [],
-    migrations: [Author1719825063867, User1719830267980, Book1719832355608],
+    migrations: [Author1719825063867, User1719830267980, Book1719832355608, Favourite1719906554069],
     synchronize: false,
     logging: false,
 })
